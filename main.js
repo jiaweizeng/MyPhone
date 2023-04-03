860e8796a79602ddea8690bf1d587fdfb85cb98e
jiaweizeng <597800295@qq.com>
2023-04-03 14:42:53 +0800

    资源图片

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      // transparent: true,
+      frame: false 
     })
   
-    win.loadFile('main.html')
+    win.loadFile('login.html')
   }
 
   app.whenReady().then(() => {
